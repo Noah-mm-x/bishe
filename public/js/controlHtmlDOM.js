f06622bdd36fcf00ffbe2838e011e151e25474e4
mfx55 <1185055083@qq.com>
2017-01-29 22:16:27 +0800

    完成首页

--- public/js/controlHtmlDOM.js
+++ public/js/controlHtmlDOM.js
@@ -3,4 +3,22 @@
  */
 $(function () {
 
+//    顶部图片切换
+    var $switchFrame = $(".image-switch-frame"), $preImage = $(".pre-image ul li"), timer = null;
+    $preImage.hover(function () {
+        var _self = $(this), showImage;
+        timer = setTimeout(function () {
+            showImage = _self.find("img").attr("src");
+            $switchFrame.fadeOut(function () {
+                $(this).css({
+                    "backgroundImage": "url("+showImage+")",
+                }).fadeIn();
+            })
+        }, 200);
+    }, function () {
+        clearTimeout(timer);
+    });
+
+// 小集体动态化
+
 });
